95e9a8ce2cb06915c813a8f2dec9fc0c40210d9e
TreadKing <devangminer722@gmail.com>
2020-12-04 00:16:28 -0500

    jank part 2 done, day 3 done janl

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -4,22 +4,44 @@ slopeOut = open('slopeOut.txt', 'a')
 
 position = 0
 hits = 0
-for line in slope:
-    if line[position] == '\n':
-        position = 0
+# for line in slope:
+#     if line[position] == '\n':
+#         position = 0
 
-    if line[position] == '#':
-        hits += 1
-        line = line[:position] + 'X' + line[position+1:]
+#     if line[position] == '#':
+#         hits += 1
+#         line = line[:position] + 'X' + line[position+1:]
  
-    else:
-        line = line[:position] + 'O' + line[position+1:]
+#     else:
+#         line = line[:position] + 'O' + line[position+1:]
 
-    position += 3
-    if position >= len(line):
-        position -= len(line)
+#     position += 7
+#     if position >= len(line):
+#         position -= len(line)
+#         position += 1
+
+#     slopeOut.write(line)
+
+count = 0
+for line in slope:
+    if count % 2 == 0:
+
+        if line[position] == '\n':
+            position = 0
+
+        if line[position] == '#':
+            hits += 1
+            line = line[:position] + 'X' + line[position+1:]
+    
+        else:
+            line = line[:position] + 'O' + line[position+1:]
+
         position += 1
+        if position >= len(line):
+            position -= len(line)
+            position += 1
 
     slopeOut.write(line)
+    count += 1
 
-print('Trees Hsit: ' + str(hits))
+print('Trees Hit: ' + str(hits))
