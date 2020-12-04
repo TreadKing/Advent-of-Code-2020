5669d726f8c04b3a1f0e32aa1cef82f5c548ce6b
TreadKing <devangminer722@gmail.com>
2020-12-04 01:04:18 -0500

    made a solution that only needs to be run once

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -1,18 +1,16 @@
 slope = open('slope.txt', 'r')
-slopeOut = open('slopeOut.txt', 'a')
+# slopeOut = open('slopeOut.txt', 'a')
 
 position1 = 0
 position2 = 0
 position3 = 0
 position4 = 0
+position5 = 0
 hits1 = 0
 hits2 = 0
 hits3 = 0
 hits4 = 0
-
-position5 = 0
-hits5 = 5
-
+hits5 = 0
 count = 0
 
 for line in slope:
@@ -66,7 +64,8 @@ for line in slope:
     if position4 >= len(line):
         position4 -= len(line)
         position4 += 1
-    
+
+
     if count % 2 == 0:
 
         if line[position5] == '\n':
@@ -74,26 +73,19 @@ for line in slope:
 
         if line[position5] == '#':
             hits5 += 1
-            line = line[:position5] + 'X' + line[position5+1:]
-    
-        else:
-            line = line[:position5] + 'O' + line[position5+1:]
 
         position5 += 1
         if position5 >= len(line):
             position5 -= len(line)
             position5 += 1
 
-    slopeOut.write(line)
+    # slopeOut.write(line)
     count += 1
 
-
-
-
-print('Trees Hist 1: ' + str(hits1))
-print('Trees Hist 2: ' + str(hits2))
-print('Trees Hist 3: ' + str(hits3))
-print('Trees Hist 4: ' + str(hits4))
-print('Trees Hist 5: ' + str(hits5))
+print('Trees Hit 1: ' + str(hits1))
+print('Trees Hit 2: ' + str(hits2))
+print('Trees Hit 3: ' + str(hits3))
+print('Trees Hit 4: ' + str(hits4))
+print('Trees Hit 5: ' + str(hits5))
 
 print('Total Trees hit: ' + str(hits1 * hits2 * hits3 * hits4 * hits5))
