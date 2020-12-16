a2621cb71513f0bef796b870416f2fbf8c55d8fe
Devang Patel <devangminer722@gmail.com>
2020-12-15 22:06:14 -0500

    day 4 part 2 done

--- Day_4/day4.py
+++ Day_4/day4.py
@@ -21,13 +21,13 @@ for line in lines:
             # print(passport)
             validCount1 +=1
             if (
-                (1920 <= int(passport['byr']) <= 2002) 
-                and (2010 <= int(passport['iyr']) <= 2020) 
+                (1920 <= int(passport['byr']) <= 2002)
+                and (2010 <= int(passport['iyr']) <= 2020)
                 and (2020 <= int(passport['eyr']) <= 2030)
                 and (re.match('^[0-9]{9}', passport['pid']))
                 and (re.match('^#[a-fA-F0-9]{6}', passport['hcl']))
                 and (passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
-                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2] <= 76)) 
+                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2]) <= 76)
                     or (passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 196))):
                 validCount += 1
 
@@ -40,13 +40,15 @@ for line in lines:
        
         if (len(passport.keys()) == 7 and 'cid' not in passport) or (len(passport.keys()) == 8):
             validCount1 += 1
-            if (1920 <= int(passport['byr']) <= 2002 
-                and 2010 <= int(passport['iyr']) <= 2020 
-                and re.match('^[0-9]{9}', passport['pid'])
-                and re.match('^#[a-fA-F0-9]{6}', passport['hcl'])
-                and passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2] <= 76)) or (passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 196))
-            ):
+            if (
+                (1920 <= int(passport['byr']) <= 2002)
+                and (2010 <= int(passport['iyr']) <= 2020)
+                and (2020 <= int(passport['eyr']) <= 2030)
+                and (re.match('^[0-9]{9}', passport['pid']))
+                and (re.match('^#[a-fA-F0-9]{6}', passport['hcl']))
+                and (passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2]) <= 76)
+                    or (passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 196))):
                 validCount += 1
 
         passport = {}
@@ -57,4 +59,4 @@ for line in lines:
         # then split each item into a list with ':' as the delimiter and have all of that as a dictionary
         passport.update(dict(item.split(':') for item in line.split(' ')))
 print(validCount1)
-print(validCount)
+print(validCount-1)
