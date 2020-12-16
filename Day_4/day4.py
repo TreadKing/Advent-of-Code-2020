cc056d8bb6c6725f9b5b87b080a493dacb038cce
Devang Patel <devangminer722@gmail.com>
2020-12-15 21:38:43 -0500

    Made some changes that prodice a wrog answer

--- Day_4/day4.py
+++ Day_4/day4.py
@@ -1,46 +1,54 @@
-file = open('test.txt', 'r')
+import re
+file = open('day4.txt', 'r')
 
 lines = file.readlines()
 last = lines[-1]
 passport = {}
 validCount = 0
+validCount1 = 0
 passKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+values = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 # read in lines into a dictionary till you hit a new line or the last line(the last line is not a new line)
 for line in lines:
 
     if line == '\n':
-        print(passport)
+        # print(passport)
         # if all(key in passKeys for key in passport.keys()):
         #     print(True)
         keys = passport.keys()
-        if len(passport.keys()) == 7 and 'cid' not in passport:
-            validCount += 1
-            print(True)
+        
+        if (len(passport.keys()) == 7 and 'cid' not in passport) or (len(passport.keys()) == 8):
+            # print(passport)
+            validCount1 +=1
+            if (
+                (1920 <= int(passport['byr']) <= 2002) 
+                and (2010 <= int(passport['iyr']) <= 2020) 
+                and (2020 <= int(passport['eyr']) <= 2030)
+                and (re.match('^[0-9]{9}', passport['pid']))
+                and (re.match('^#[a-fA-F0-9]{6}', passport['hcl']))
+                and (passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2] <= 76)) 
+                    or (passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 196))):
+                validCount += 1
 
-        elif len(passport.keys()) == 8:
-            validCount += 1
-            print(True)
 
-        else:
-            print(False)
         passport = {}
 
     elif line is last:
         line = line.strip('\n')
         passport.update(dict(item.split(':') for item in line.split(' ')))
-        print(passport)
-        # if all(key in passKeys for key in passport.keys()):
-        #     print(True)
-        if len(passport.keys()) == 7 and 'cid' in passport:
-            validCount += 1
-            print(True)
+       
+        if (len(passport.keys()) == 7 and 'cid' not in passport) or (len(passport.keys()) == 8):
+            validCount1 += 1
+            if (1920 <= int(passport['byr']) <= 2002 
+                and 2010 <= int(passport['iyr']) <= 2020 
+                and re.match('^[0-9]{9}', passport['pid'])
+                and re.match('^#[a-fA-F0-9]{6}', passport['hcl'])
+                and passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+                and ((passport['hgt'][-2:] == 'in' and 59 <= int(passport['hgt'][:-2] <= 76)) or (passport['hgt'][-2:] == 'cm' and 150 <= int(passport['hgt'][:-2]) <= 196))
+            ):
+                validCount += 1
 
-        elif len(passport.keys()) == 8:
-            validCount += 1
-            print(True)
-        
-        else:
-            print(False)
         passport = {}
 
     else:
@@ -48,7 +56,5 @@ for line in lines:
         # split up the string into a list of items with ' ' as the delimiter
         # then split each item into a list with ':' as the delimiter and have all of that as a dictionary
         passport.update(dict(item.split(':') for item in line.split(' ')))
-
+print(validCount1)
 print(validCount)
-        
-
