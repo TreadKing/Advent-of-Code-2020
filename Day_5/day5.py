5c165a4900c9e7658ee40d10a9630cf179c6bca3
Devang Patel <devangminer722@gmail.com>
2020-12-17 01:51:24 -0500

    day 5 part 2 done

--- Day_5/day5.py
+++ Day_5/day5.py
@@ -1,4 +1,6 @@
+import math
 def findRow(ticket):
+
     highRows = 127
     lowRows = 0
     midRows = 0
@@ -6,28 +8,31 @@ def findRow(ticket):
     lowCols = 0
     midCols = 0
     for i in range(len(ticket)):
-
-        midRows = (highRows + lowRows) // 2
-        midCols = (highCols + lowCols) // 2
         if ticket[i] == 'F':
-            highRows = midRows - 1
+            highRows = highRows - int(highRows - lowRows + 1) /2
 
         elif ticket[i] == 'B':
-            lowRows = midRows + 1
+            lowRows = lowRows + int(highRows - lowRows + 1) /2
 
         elif ticket[i] == 'L':
-            highCols = midCols - 1
+            highCols = highCols - int(highCols - lowCols + 1) /2
         
         elif ticket[i] == 'R':
-            lowCols = midCols + 1
+            lowCols = lowCols + int(highCols - lowCols + 1) /2
 
-    return ((midRows + 1 )* 8) + midCols
+    return ((lowRows)* 8) + lowCols
 
+seatsIds = []
 with open('seats.txt', 'r') as seats:
     max = 0
     for seat in seats:
         seatId = findRow(seat.strip())
+        seatsIds.append(seatId)
         if seatId > max:
             max = seatId
-
+    
+    seatsIds.sort()
+    # part 2
+    print(set(range(seatsIds[0], seatsIds[len(seatsIds)-1]+1)) - set(seatsIds) )
+    # part 1
     print(max)
