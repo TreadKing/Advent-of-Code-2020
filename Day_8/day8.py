4699881fb5e5fef9a66bcbd7fe4b9d60e8d9c608
TreadKing <devangminer722@gmail.com>
2020-12-23 20:23:47 -0500

    day 8 done

--- Day_8/day8.py
+++ Day_8/day8.py
@@ -6,7 +6,6 @@ file.close()
 
 acc = 0
 line = 0
-overlap = False
 lines_seen = {}
 
 while line not in lines_seen:
@@ -16,14 +15,56 @@ while line not in lines_seen:
     if boot[line][0:3] == 'acc':
         acc += int(boot[line][4:])
         line += 1
-        print(line)
 
     elif boot[line][0:3] == 'jmp':
         line += int(boot[line][4:])
-        print(line)
-    
+
     else:
         line += 1
-        print(line)
 
+# part 1
 print(acc)
+
+
+# part 2
+
+def testBoot(bootLines):
+
+    accumulator = 0
+    line = 0
+    lines_seen = {}
+    
+    while line not in lines_seen and line < len(bootLines):
+
+        lines_seen[line] = bootLines[line]
+
+        if bootLines[line][0:3] == 'acc':
+            accumulator += int(bootLines[line][4:])
+            line += 1
+
+        elif bootLines[line][0:3] == 'jmp':
+            line += int(bootLines[line][4:])
+
+        else:
+            line += 1
+
+    if line in lines_seen:
+        return 0
+    else:
+        return accumulator
+
+
+# testing line changes
+for i, item in enumerate(boot):
+    if item[0:3] == 'jmp':
+        boot[i] = 'nop' + item[3:]
+        output = testBoot(boot)
+        if output !=  0: print(output)
+        boot[i] = item
+    
+    if item[0:3] == 'nop':
+        boot[i] = 'jmp' + item[3:]
+        output = testBoot(boot)
+        if output !=  0: print(output)
+        boot[i] = item
+    
